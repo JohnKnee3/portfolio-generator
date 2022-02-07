af457f4a53323d59e755c0e6a67c6efe241fe94c
john.a.clark3 <john.a.clark3@gmail.com>
2022-02-07 12:36:49 -0700

    9.1.5
    can not update  a regular const like
    const message = 'Hello Node!';
    message = 'Goodbye Node!';
    
    const sum = 5 + 3;
    sum += 1;
    
    but can update anything inside of a const for an array or an object
    const animalArray = ['dog', 'cat', 'pig'];
    
    animalArray.push('cow');
    
    const personObj = {
      name: 'Lernantino',
      age: 99
    };
    
    personObj.age = 100;
    personObj.occupation = 'Developer';
    
    for example cow would get added, the age would get changed to 100 and occupation would be added.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello Node!");
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+    name: 'Lernantino',
+    age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
+
+console.log(animalArray);
+console.log(personObj);
